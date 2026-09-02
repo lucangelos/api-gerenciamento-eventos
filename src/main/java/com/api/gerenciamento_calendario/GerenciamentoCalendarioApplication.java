5575d4d25b23fc9153f3d5c46531aedd96c461f5
lucangelos <lucasangeloss27@gmail.com>
2026-09-02 19:49:30 -0300

    Criando repositório

---
+++ src/main/java/com/api/gerenciamento_calendario/GerenciamentoCalendarioApplication.java
@@ -0,0 +1,13 @@
+package com.api.gerenciamento_calendario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoCalendarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoCalendarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/gerenciamento_calendario/GerenciamentoCalendarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gerenciamento_calendario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoCalendarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
